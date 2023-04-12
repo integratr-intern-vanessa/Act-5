1c2c5258cdc46d28830536a5e0f52ebf082c3537
Vanessa rose Consigna <vconsigna.intern.integratr@outlook.com>
2023-04-12 18:11:41 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const container = document.getElementById('input-cont');
+
+    function AddInput() { 
+    let input = document.createElement('input');
+    input.placeholder = 'Type something';
+    container.appendChild(input);
+            
+}
